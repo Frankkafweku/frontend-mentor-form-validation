0d500de832b92d46c96165e9ab1a845bf41b7d7f
Frank Kafweku <frankkafweku84@gmail.com>
2023-08-28 10:40:22 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const form = document.getElementById("form");
+const submitBtn = document.getElementById("submit-btn");
+const email = document.getElementById("email");
+const errors = document.getElementById("error-msg");
+const sentTo = document.getElementById("sent-to")
+const dismissMsg = document.getElementById("dismiss");
+
+form.addEventListener('submit',(e)=>{
+    let errorMessages =[];
+    e.preventDefault();
+
+    if(email.value === "" || email.value === null){
+        errorMessages.push("email must not be empty!");
+    } else if (!email.value.includes("@")){
+        errorMessages.push("invalid Email");
+    
+
+
+    } 
+    if (errorMessages.length > 0) {
+        errors.innerText = errorMessages.join(", ");
+    } else window.location.href = 'page2.html';
+    
+});
+
+function dismiss(){
+alert("hello")
+}
+dismissMsg.addEventListener(click,dismiss() )
